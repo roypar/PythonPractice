d4834b0a1993d21b1ef48d23c28037a09f66d3ed
Parijat <parijat8@gmail.com>
2020-08-25 21:24:01 +0530

    Implementing different types of attributes and methods

--- classPractice.py
+++ classPractice.py
@@ -1,3 +1,4 @@
+# Trying out out of class instance attribute assignment
 class Person:
     def __init__(self, name="John Doe", age=35, height=173):
         self.name = name
@@ -10,5 +11,36 @@ class Person:
 
 myself = Person("Parijat")
 myself.weight = 65
-print(myself)
-print(myself.weight)
+# print(myself)
+# print(myself.weight)
+
+#Trying out Class vs Instance attributes
+#Trying out instance vs class vs static method
+class Players:
+    teamMembers = []
+    teamname = 'Liverpool'
+
+    @staticmethod
+    def getBMI(height, weight):
+        return weight/(height*height)
+
+    @classmethod
+    def getClassname(cls):
+        return(cls.teamname)
+
+    def __init__(self, name, salary=0):
+        self.name = name
+        self.teamMembers.append(self.name)
+        self.__salary = salary
+
+
+p1 = Players('Mark', 1000)
+p2 = Players('Steve', 2000)
+p3 = Players('Parijat', 500)
+
+# print(f"{p1.name} has {p1.teamMembers} as teammates")
+# print(f"{p2.name} has {p2.teamMembers} as teammates")
+# print(Players.getClassname())
+# print(Players.getBMI(173, 72))
+print(p3._Players__salary)
+
